2e0e33c5b34864aa3aa78d1ee6747322fbfa981c
devintachr <dee17devinta.dc@gmail.com>
2015-10-02 17:26:37 +0700

    MENGGANTI WARNA BACKGROUND

---
+++ Devinta Christanti_1441180170/WindowsApplication2/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+    Private Sub Ubah_Click(sender As Object, e As EventArgs) Handles Ubah.Click
+        If Me.BackColor = Color.LightCoral Then
+            Me.BackColor = Color.CornflowerBlue
+        ElseIf Me.BackColor = Color.CornflowerBlue Then
+            Me.BackColor = Color.LightCoral
+        End If
+    End Sub
+
+End Class
